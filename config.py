d5693da578fe0afffcc60d3c5e73b3f57a1591db
unknown <korkorshiela@gmail.com>
2021-03-16 20:50:53 -0400

    falling interest rates

---
+++ config.py
@@ -0,0 +1,9 @@
+API_key="wJwp9NFb-QWNy3d1f9_w"
+
+
+#cloud aws database
+remote_db_endpoint = 'databootcamp.ct51fgasslz1.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'team_logic'
+remote_db_pwd = 'Dartlings1!'
+remote_db_name = 'team_logic'
